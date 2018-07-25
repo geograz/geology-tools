b6c8f3e1452e203d5d847437939c81f63bc0afe1
geograz <32653899+geograz@users.noreply.github.com>
2018-07-25 09:12:06 +0200

    Update mean orientation calculator.py

--- mean orientation calculator.py
+++ mean orientation calculator.py
@@ -1,37 +1,29 @@
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.art3d as art3d
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib.patches import Circle, Wedge, PathPatch
-import mpl_toolkits.mplot3d.art3d as art3d
 
-import csv
-from math import sin, cos, radians, sqrt, atan2, pi, acos
+import numpy as np
+import pandas as pd
 
-### function definition ###
-def normal_vectors(data_import):
-    n_X = []
-    n_Y = []
-    n_Z = []
+
+##### functions #####
+def normal_vectors(dips, dipdirs):
     
-    for i in range(len(data_import[0])):
-        dip = float(data_import[0][i])
-        dipdir = float(data_import[1][i])
-        X = sin(radians(dip)) * sin(radians(dipdir))
-        Y = sin(radians(dip)) * cos(radians(dipdir))
-        Z = cos(radians(dip))
-        n_X.append(X)
-        n_Y.append(Y)
-        n_Z.append(Z)
-    
+    n_X = np.sin(np.radians(dips)) * np.sin(np.radians(dipdirs))
+    n_Y = np.sin(np.radians(dips)) * np.cos(np.radians(dipdirs))
+    n_Z = np.cos(np.radians(dips))
+   
     return n_X, n_Y, n_Z
         
 def mean_unit_vector(normal_vectors):
     # summing up of normal vector coordinates ####
-    sum_n_X = round(sum(normal_vectors[0]), 10)
-    sum_n_Y = round(sum(normal_vectors[1]), 10)
-    sum_n_Z = round(sum(normal_vectors[2]), 10)
+    sum_n_X = sum(normal_vectors[0])
+    sum_n_Y = sum(normal_vectors[1])
+    sum_n_Z = sum(normal_vectors[2])
     
     ### calculating mean unit vector ### 
-    norm_vector = sqrt(sum_n_X**2 + sum_n_Y**2 + sum_n_Z**2)
+    norm_vector = np.sqrt(sum_n_X**2 + sum_n_Y**2 + sum_n_Z**2)
     norm_n_X = sum_n_X / norm_vector
     norm_n_Y = sum_n_Y / norm_vector
     norm_n_Z = sum_n_Z / norm_vector
@@ -39,66 +31,59 @@ def mean_unit_vector(normal_vectors):
     return norm_n_X, norm_n_Y, norm_n_Z
         
 def backcalculation(mean_unit_vector): # backcalculation to dipdirection and dip
-    mean_DipDir = (360 - atan2(mean_unit_vector[1], mean_unit_vector[0]) * (180/pi))+90
-    mean_Dip = acos(mean_unit_vector[2])*(180/pi)
+    mean_DipDir = (360 - np.arctan2(mean_unit_vector[1], mean_unit_vector[0]) * (180/np.pi))+90
+    mean_Dip = np.arccos(mean_unit_vector[2])*(180/np.pi)
     
-    return mean_DipDir, mean_Dip
+    return round(mean_DipDir, 1), round(mean_Dip,1)
         
 def print_result(backcalculation):
     backcalculation = list(backcalculation)
     if backcalculation[0] > 360:
         backcalculation[0] = backcalculation[0]-360
-    print('mean orientation: (', round((backcalculation[0]),1),'/',round((backcalculation[1]),1),')')
+    print('mean orientation: ({}/{})'.format(backcalculation[0],backcalculation[1]))
     if backcalculation[0] > 180:
         backcalculation[0] = backcalculation[0]-360
-    print('mean orientation pole: (', round((backcalculation[0]+180),1),'/',round((90-backcalculation[1]),1),')')
+    print('mean orientation pole: ({}/{})'.format(backcalculation[0] + 180, 90 - backcalculation[1]))
 
 
-### execution ####
-print('''Application for computation of the mean orientation of structural geological data via vector summation from .csv spreadsheets.\n
-A 3D, lower hemisphere pole point projection will be given for result inspection.\n
-NOTE: App will skip first row\n''')
+##### data import #####
+print('''Calculate the mean orientation of structural geological data 
+      via vector summation from .csv spreadsheets.
+      Check the results within the 3D, lower hemisphere pole point projection!
+      NOTE: App will skip first row\n''')
 
-File_Path = (input(r'filepath to .csv: '))
+File_Path = input(r'filepath to .csv: ')
 File_Path = File_Path.replace( '\\' , '/' )
 
 delim = input('delimiter: ')
-          
-with open(File_Path) as f:
-    next(f) #skip first row
-    readCSV = csv.reader(f, delimiter=delim)
+col_dip = input('name of column with dip: ')
+col_dipdir = input('name of column with dip direction: ')
 
-    dips = []
-    dipdirs = []
-    
-    col_dip = int(input('Column with dip: '))-1
-    col_dipdir = int(input('Column with dipdirection: '))-1
-    
-    # separates orientations
-    for row in readCSV:
-        a = int(float(row[col_dip]))
-        b = int(float(row[col_dipdir]))
-        dips.append(a)
-        dipdirs.append(b)         
-                
-data_import = [dips]+[dipdirs]
-             
-print((print_result(backcalculation(mean_unit_vector(normal_vectors(data_import))))))
+df = pd.read_csv(File_Path, delimiter = delim, usecols = [col_dip, col_dipdir],
+                 encoding = 'iso8859_15') #with encoding "iso8859_15" pandas is able to read letters like "Ä,Ö,Ü" that are used in German...
+
+##### calculations #####
+print_result(backcalculation(mean_unit_vector(normal_vectors(df[col_dip], df[col_dipdir]))))
 
 
-### 3D plot ###
-n_X = [i*-1 for i in normal_vectors(data_import)[0]] # *-1 for lower hemisphere projection
-n_Y = [i*-1 for i in normal_vectors(data_import)[1]] # *-1 for lower hemisphere projection
-n_Z = [i*-1 for i in normal_vectors(data_import)[2]] # *-1 for lower hemisphere projection
-
+##### 3D plot #####
+n_X = normal_vectors(df[col_dip], df[col_dipdir])[0] * -1 # *-1 for lower hemisphere projection
+n_Y = normal_vectors(df[col_dip], df[col_dipdir])[1] * -1
+n_Z = normal_vectors(df[col_dip], df[col_dipdir])[2] * -1
 
 fig = plt.figure(figsize = (9,9))
 ax1 = fig.add_subplot(111, projection = '3d')
 
 ax1.scatter(n_X, n_Y, n_Z,s = 2, color = 'black') #pole points
-ax1.plot_wireframe([0, mean_unit_vector(normal_vectors(data_import))[0]*-2], [0, mean_unit_vector(normal_vectors(data_import))[1]*-2], [0, mean_unit_vector(normal_vectors(data_import))[2]*-2], color = 'red') # mean orientation polevector
 
-ax1.plot_wireframe([0,0,0.25,-0.25,0], [-1,1,0.75,0.75,1], [-1.5,-1.5,-1.5,-1.5,-1.5], color = 'black') #North arrow
+ax1.plot([0, mean_unit_vector(normal_vectors(df[col_dip], df[col_dipdir]))[0]*-2],
+          [0, mean_unit_vector(normal_vectors(df[col_dip], df[col_dipdir]))[1]*-2],
+          [0, mean_unit_vector(normal_vectors(df[col_dip], df[col_dipdir]))[2]*-2],
+          color = 'red') # mean orientation polevector
+
+
+##### draw stereonet #####
+ax1.plot([0,0,0.25,-0.25,0], [-1,1,0.75,0.75,1], [-1.5,-1.5,-1.5,-1.5,-1.5], color = 'black') #North arrow
 a = Wedge((0,0),1,180,0,  edgecolor = 'black', facecolor = 'none', linewidth = 0.1) #stereonet
 b = Wedge((0,0),1,180,0,  edgecolor = 'black', facecolor = 'none', linewidth = 0.1) #stereonet
 p = Circle((0,0), 1, edgecolor = 'black', facecolor = 'none', linewidth = 0.1) #stereonet
@@ -108,7 +93,7 @@ ax1.add_patch(p) #stereonet
 art3d.pathpatch_2d_to_3d(a, z=0, zdir="x") #stereonet
 art3d.pathpatch_2d_to_3d(b, z=0, zdir="y") #stereonet
 art3d.pathpatch_2d_to_3d(p, z=0, zdir="z") #stereonet
-ax1.plot_wireframe([0,0],[0,0],[0,-1], linewidth = 0.1 , color = 'black') #stereonet
+ax1.plot([0,0],[0,0],[0,-1], linewidth = 0.1 , color = 'black') #stereonet
 
 ax1.set_axis_off()
 
