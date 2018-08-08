62078fa0be3d71ed09afb9e8731db55a42a957c9
geograz <32653899+geograz@users.noreply.github.com>
2018-08-08 20:39:34 +0200

    Update apparent dip calculator.py

--- apparent dip calculator.py
+++ apparent dip calculator.py
@@ -10,7 +10,7 @@ root = tk.Tk()
 root.title('Apparent Dip Calculator')
 
 header = tk.Label(root, text='Apparent Dip Calculator', font=HEADER_FONT)
-header.grid(row=0, columnspan=2)
+header.grid(row=0, columnspan = 4)
 
 DipDir_label = ttk.Label(root, text='real dip direction:')
 DipDir_label.grid(row=1, column=0, sticky='E')
@@ -28,13 +28,22 @@ Cs_entry = ttk.Entry(root)
 Cs_entry.grid(row=3, column=1, sticky='W')
 
 root.cv = tk.Canvas(root, width = 200, height = 200)
-root.cv.grid(row=7, column=2)
+root.cv.grid(row=7, column=3)
 
 def graphics(appdip): #creates graphical representation of apparent dip angle
-    root.cv.create_arc(10, 10, 190, 190, start=90, extent=-appdip, dash=(7,4), fill='white')
-    root.cv.create_arc(10, 10, 190, 190, start=90, extent= 360 - appdip, dash=(7,4), fill='#f0f0f0')
+    root.cv.create_arc(10, 10, 190, 190, 
+                       start=90, 
+                       extent = -appdip, 
+                       dash=(7,4), 
+                       fill='white')
+    root.cv.create_arc(10, 10, 190, 190, 
+                       start=90, 
+                       extent = 360 - appdip, 
+                       dash=(7,4), 
+                       fill='#f0f0f0')
     root.cv.create_oval(10, 10, 190, 190)
-    root.cv.create_line(100, 0, 100, 30, width=3)
+    root.cv.create_line(100, 0, 100, 30, 
+                        width=3)
 
 def calc_appdip(graphics):
     def redirector(inputStr):  # permits printing on GUI
@@ -65,13 +74,19 @@ def calc_appdip(graphics):
 Go_button = ttk.Button(root, text='GO', command= lambda: calc_appdip(graphics))
 Go_button.grid(row=5, columnspan=2)
 
-expl_label = ttk.Label(root, text='resulting apparent dip is given as ° from vertical - clockwise')
+expl_label = ttk.Label(root, text = 'resulting apparent dip is given as ° from vertical (clockwise)')
 expl_label.grid(pady = 10, row=6, columnspan = 2)
 
-textbox = tk.Text(root, height = 20, width = 50)
-textbox.grid(row=7, columnspan=2)
+textbox = tk.Text(root, height = 20, width = 40)
+textbox.grid(row=7, columnspan = 2)
 
-exitbutton = ttk.Button(root, text='Close', command = lambda: exit())
-exitbutton.grid(columnspan = 2)
+scrollb = tk.Scrollbar(width = 15, command = textbox.yview)
+scrollb.grid(row = 7, column = 2, sticky = 'NS')
+
+clear_button = (ttk.Button(root, text = 'Clear', command = lambda: textbox.delete(1.0, tk.END)))
+clear_button.grid(columnspan = 2)
+
+exit_button = ttk.Button(root, text='Close app', command = lambda: exit())
+exit_button.grid(columnspan = 2)
 
 root.mainloop()
