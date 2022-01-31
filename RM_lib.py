6c4f4872e85daa847dd4bfaf976cb68f68f2a110
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2022-01-31 14:05:05 +0100

    Add rockmass strength computation codes
    Adding files to compute rockmass strength and deformation parameters acc. to Hoek et al. (2002) and other authors.

---
+++ RM_lib.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 14:00:44 2021
+author: Georg H. Erharter
+
+Python script that computes rockmass parameters for a set of multiple input
+parameters based on the Hoek Brown failure criterion. An application would be
+if multiple sets of rockmass parameters for different overburdens and / or
+different levels of rockmass disturbance should be computed. The code creates
+two excel files where one contains the input parameters and one the output
+parameters.
+
+"RM_lib.py" is the custom library for the file "RM_main.py".
+
+No guarantee is given on the flawless functionality of the code and the code is
+licensed under the MIT License (see license file in repository).
+Parts of the code are based on: http://geologyandpython.com/hoek-brown.html
+"""
+
+from itertools import product
+import numpy as np
+import pandas as pd
+from typing import List
+
+
+class Utilities:
+    '''class that contains general purpose functions'''
+
+    def __init__(self):
+        pass
+
+    def get_combinations(self, dictionary: dict, name: str) -> pd.DataFrame:
+        '''compute all possible combinations of lists that are
+        values of a dictionary'''
+        combinations = list(product(*list([dictionary['intact UCS [MPa]'],
+                                           dictionary['GSI'],
+                                           dictionary['mi'],
+                                           dictionary['disturbance factor'],
+                                           dictionary['intact modulus - Ei [MPa]'],
+                                           dictionary['unit weight [MN/m³]'],
+                                           dictionary['tunnel depth [m]']])))
+        # save combinations
+        df = pd.DataFrame(np.array(combinations), columns=dictionary.keys())
+        df.to_excel(f'{name}_input.xlsx', index=True)
+        return df
+
+
+class Hoek:
+    '''class for computation of the Hoek Brown failure criterium according to
+    Hoek, E., Carranza-Torres, C. and Corkum, B. (2002),
+    “Hoek-Brown failure criterion – 2002 Edition”, Toronto.'''
+
+    def __init__(self):
+        pass
+
+    def HoekBrownCriterion(self, mi: int, GSI: int, D: float) -> List:
+        mb = mi * np.exp((GSI-100)/(28-(14*D)))
+        s = np.exp((GSI-100)/(9-(3*D)))
+        a = (1/2)+(1/6)*(np.exp(-GSI/15)-np.exp(-20/3))
+        return mb, s, a
+
+    def FailureEnvelopeRange(self, sigci: float, mb: float, s: float, a: float,
+                             unit_weigth: float, depth: float) -> List:
+        sigcm = sigci * (mb+4*s-a*(mb-8*s))*(((mb/4)+s)**(a-1))/(2*(1+a)*(2+a))
+        sig3_max = sigcm * 0.47 * (sigcm / (unit_weigth * depth))**(-.94)
+        return sigcm, sig3_max
+
+    def MohrCoulombFit(self, sig3_max: float, sigci: float, a: float,
+                       mb: float, s: float) -> List:
+        sig3n = sig3_max / sigci
+        phi = np.rad2deg(np.arcsin((6*a*mb*((s+mb*sig3n)**(a-1)))/(2*(1+a)*(2+a)+6*a*mb*((s+mb*sig3n)**(a-1)))))
+
+        coh_term1 = sigci*((1+2*a)*s+(1-a)*mb*sig3n)*((s+mb*sig3n)**(a-1))
+        coh_term2 = (1+a)*(2+a)*np.sqrt(1+(6*a*mb*((s+mb*sig3n)**(a-1)))/((1+a)*(2+a)))
+        coh = coh_term1 / coh_term2
+        return phi, coh
+
+    def RMStrength(self, sigci: float, s: float, a: float, mb: float) -> List:
+        sigc = sigci*s**a
+        sigtm = (s * sigci)/mb*-1
+        return sigc, sigtm
+
+
+class Deformation:
+    '''class that contains functions to compute the rockmass deformation moduli
+    according to various authors'''
+
+    def __init__(self):
+        pass
+
+    def RMDef_Hoek(self, sigci: float, D: float, GSI: int, Ei: float,
+                   paper='Hoek & Diederichs (2006)') -> float:
+        '''2 ways to compute the rockmass deformation modulus acc. to Hoek are
+        implemented:
+        Hoek, E., Carranza-Torres, C. and Corkum, B. (2002),
+        “Hoek-Brown failure criterion – 2002 Edition”, Toronto.
+
+        and the improved version:
+        Hoek, E. and Diederichs, M.S. (2006), “Empirical estimation of rock
+        mass modulus”, International Journal of Rock Mechanics and Mining
+        Sciences, Vol. 43 No. 2, pp. 203–215.'''
+        if paper == 'Hoek et al. (2002)':
+            # rockmass deformation modulus after Hoek et al. (2002)
+            Erm = (1 - (D/2)) * (np.sqrt(sigci/100.0)*10**((GSI-10)/40.0)) * 10**3
+        elif paper == 'Hoek & Diederichs (2006)':
+            # rockmass deformation modulus after Hoek & Diederichs (2006)
+            Erm = Ei*(0.02+((1-D/2)/(1+np.exp((60+15*D-GSI)/11))))
+        return Erm
+
+    def RMDef_Arora1987(self):
+        '''not yet implemented way to compute the rockmass deformation modulus
+        acc. to
+        Arora, Vijay Kumar. “Strength and deformational behaviour of jointed
+        rocks.” (1987).'''
+        pass
+
+    def RMDef_Verman1997(self, GSI, depth):
+        '''computation of the rockmass deformation modulus acc. to
+        Verman, Manoj, Bhawani Singh, M. N. Viladkar and Jaydev Jethwa. “Effect
+        of tunnel depth on modulus of deformation of rock mass.” Rock Mechanics
+        and Rock Engineering 30 (1997): 121-127.'''
+        RMR = GSI  # according to Saroglou et al. 2019
+        # alpha = 0.3 and 0.16 at RMR = 68 and 31, respectively
+        alpha = np.interp(RMR, [31, 68], [0.16, 0.3])
+        Erm = 0.4*(depth**alpha)*10**((RMR-20)/38)  # GPa
+        return Erm * 1000  # MPa
+
+    def RMDef_AsefReddish2002(self, sigc, E_0, sig_cm, unit_weigth, depth,
+                              k0):
+        '''computation of the rockmass deformation modulus acc. to
+        Asef, Mohammad Reza and David J. Reddish. “The impact of confining
+        stress on the rock mass deformation modulus.” Geotechnique 52 (2002):
+        235-241.'''
+        # sigc: unconfined compressive strength of the intact rock
+        # E_0: deformation modulus of the jointed rock mass at UCS
+        # sig3: sigma3 = sigma2 triaxial stress
+        # sig_cm: unconfined compressive strength of the jointed rock mass
+        sig1 = unit_weigth * depth
+        sig3 = sig1 * k0
+
+        b = 15 + 60*np.exp(-0.18*sigc)
+        term1 = 200*(sig3 / sig_cm) + b
+
+        term2 = (sig3 / sig_cm) + b
+        Erm = (E_0/1000) * (term1 / term2)
+        return Erm * 1000
---
+++ RM_main.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 14:00:44 2021
+author: Georg H. Erharter
+
+Python script that computes rockmass parameters for a set of multiple input
+parameters based on the Hoek Brown failure criterion. An application would be
+if multiple sets of rockmass parameters for different overburdens and / or
+different levels of rockmass disturbance should be computed. The code creates
+two excel files where one contains the input parameters and one the output
+parameters.
+
+Code requires the custom library "RM_lib.py" to work.
+
+No guarantee is given on the flawless functionality of the code and the code is
+licensed under the MIT License (see license file in repository).
+Parts of the code are based on: http://geologyandpython.com/hoek-brown.html
+"""
+
+import numpy as np
+import pandas as pd
+
+from RM_lib import Hoek, Utilities, Deformation
+
+hoek = Hoek()
+utils = Utilities()
+deform = Deformation()
+
+
+###############################################################################
+# dictionary with input parameters of rockmass type to test
+
+NAME = 'rt1'  # name of rockmass type and filename to save results
+inputs = {'intact UCS [MPa]': [30, 35],  # sigci
+          'GSI': [50],  # GSI
+          'mi': [12],  # mi
+          'disturbance factor': [0.0, 0.2],  # D
+          'intact modulus - Ei [MPa]': [12000],  # Ei
+          'unit weight [MN/m³]': [0.026],  # unit weight
+          'tunnel depth [m]': [10, 50, 100]}  # tunnel depth / overburden
+
+
+###############################################################################
+# compute rockmass parameters for all possible parameter combinations
+
+df_combinations = utils.get_combinations(inputs, NAME)
+
+# create new dataframe that collects results
+df_output = pd.DataFrame({'mb': [], 's': [], 'a': [], 'sig3max [MPa]': [],
+                          'cohesion [MPa]': [], 'friction angle [°]': [],
+                          'RM tensile strength [MPa]': [], 'RM UCS [MPa]': [],
+                          'RM global strength [MPa]': [],
+                          'RM DefMod Hoek&al2002 [MPa]': [],
+                          'RM DefMod Hoek&Diederichs2006 [MPa]': [],
+                          'RM DefMod Verman&al1997': [],
+                          'RM DefMod Asef&Reddish2002 [MPa]': []
+                          })
+
+# main loop that iterates through all possible combinations
+for i in df_combinations.index:
+    print(f'combination: {i}')
+    c = df_combinations.iloc[i]
+
+    mb, s, a = hoek.HoekBrownCriterion(c['mi'], c['GSI'],
+                                       c['disturbance factor'])
+
+    sigcm, sig3_max = hoek.FailureEnvelopeRange(c['intact UCS [MPa]'], mb, s,
+                                                a, c['unit weight [MN/m³]'],
+                                                c['tunnel depth [m]'])
+
+    phi, coh = hoek.MohrCoulombFit(sig3_max, c['intact UCS [MPa]'], a, mb, s)
+
+    sigc, sigtm = hoek.RMStrength(c['intact UCS [MPa]'], s, a, mb)
+
+    ERM_Hoek_0 = deform.RMDef_Hoek(c['intact UCS [MPa]'],
+                                   c['disturbance factor'],
+                                   c['GSI'], c['intact modulus - Ei [MPa]'],
+                                   paper='Hoek et al. (2002)')
+
+    ERM_Hoek_1 = deform.RMDef_Hoek(c['intact UCS [MPa]'],
+                                   c['disturbance factor'],
+                                   c['GSI'], c['intact modulus - Ei [MPa]'],
+                                   paper='Hoek & Diederichs (2006)')
+
+    ERM_Verman = deform.RMDef_Verman1997(c['GSI'], c['tunnel depth [m]'])
+
+    ERM_AsefReddish2002 = deform.RMDef_AsefReddish2002(c['intact UCS [MPa]'],
+                                                       400,
+                                                       sigc,
+                                                       c['unit weight [MN/m³]'],
+                                                       c['tunnel depth [m]'],
+                                                       k0=0.33)
+
+    df_temp = pd.DataFrame({'mb': [mb], 's': [s], 'a': [a],
+                            'sig3max [MPa]': [sig3_max],
+                            'cohesion [MPa]': [coh],
+                            'friction angle [°]': [phi],
+                            'RM tensile strength [MPa]': [sigtm],
+                            'RM UCS [MPa]': [sigc],
+                            'RM global strength [MPa]': [sigcm],
+                            'RM DefMod Hoek&al2002 [MPa]': [ERM_Hoek_0],
+                            'RM DefMod Hoek&Diederichs2006 [MPa]': [ERM_Hoek_1],
+                            'RM DefMod Verman&al1997': [ERM_Verman],
+                            'RM DefMod Asef&Reddish2002 [MPa]': [ERM_AsefReddish2002]
+                            })
+    df_output = df_output.append(df_temp)
+
+df_output.index = df_combinations.index
+# save results to excel file
+df_output.to_excel(f'{NAME}_output.xlsx', index=True)
